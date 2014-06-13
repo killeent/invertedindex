18d56adc5dc857f908172c38a0226c7f4e2358c6
Trevor Killeen <killeent@cs.washington.edu>
2014-06-13 16:22:49 -0700

    add a number of tests for tokenization

--- textProcessor_test.go
+++ textProcessor_test.go
@@ -2,31 +2,64 @@ package invertedindex
 
 import (
 	// "fmt"
+	"bytes"
 	"testing"
 )
 
-func TestEmptyFile(t *testing.T) {
-	bytes := make([]byte, 0)
-	tokens := ExtractTerms(bytes)
-	if len(tokens) != 0 {
-		t.Error("Empty byte slice should return no tokens")
+// Tokenization Tests
+
+// checks whether two 2-dimensional byte slices are equal
+func assertEqualTokenSlices(t *testing.T, a, e [][]byte) {
+	if len(a) != len(e) {
+		t.Errorf("Expected number of tokens: %d Actual: %d", len(e), len(a))
+	}
+	for i, token := range a {
+		if !bytes.Equal(token, e[i]) {
+			t.Error("Tokens not equal")
+		}
 	}
 }
 
-// Tokenization Tests
+func TestEmptyFile(t *testing.T) {
+	bytes := make([]byte, 0)
+	assertEqualTokenSlices(t, tokenize(bytes), [][]byte{})
+}
 
 func TestWhiteSpace(t *testing.T) {
 	bytes := []byte(" ")
-	tokens := ExtractTerms(bytes)
-	if len(tokens) != 0 {
-		t.Error("Whitespace only byte slice should return no tokens")
-	}
+	assertEqualTokenSlices(t, tokenize(bytes), [][]byte{})
+}
+
+func TestNewLine(t *testing.T) {
+	bytes := []byte("\n")
+	assertEqualTokenSlices(t, tokenize(bytes), [][]byte{})
+}
+
+func TestTab(t *testing.T) {
+	bytes := []byte("\t")
+	assertEqualTokenSlices(t, tokenize(bytes), [][]byte{})
 }
 
 func TestSingleTerm(t *testing.T) {
 	bytes := []byte("hi")
-	tokens := ExtractTerms(bytes)
-	if len(tokens) != 1 {
-		t.Error("Single term byte slice should return a single token")
-	}
+	expected := [][]byte{[]byte("hi")}
+	assertEqualTokenSlices(t, tokenize(bytes), expected)
+}
+
+func TestSingleTermSurroundingWhiteSpace(t *testing.T) {
+	bytes := []byte("  hi ")
+	expected := [][]byte{[]byte("hi")}
+	assertEqualTokenSlices(t, tokenize(bytes), expected)
+}
+
+func TestMultipleTerms(t *testing.T) {
+	bytes := []byte("hi there")
+	expected := [][]byte{[]byte("hi"), []byte("there")}
+	assertEqualTokenSlices(t, tokenize(bytes), expected)
+}
+
+func TestMultipleTermsWhiteSpace(t *testing.T) {
+	bytes := []byte(" hi there  \n")
+	expected := [][]byte{[]byte("hi"), []byte("there")}
+	assertEqualTokenSlices(t, tokenize(bytes), expected)
 }

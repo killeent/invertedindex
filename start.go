a98092e52b38975ee46652d5cc32604e7aedfee2
Trevor Killeen <killeent@cs.washington.edu>
2014-06-13 10:43:14 -0700

    split directory reading into functions

--- start.go
+++ start.go
@@ -1,4 +1,6 @@
 /**
+Original Author: Trevor Killeen (2014)
+
 Given a file or directory, reads the contents of that file or the
 files in the directory into memory.
 */
@@ -11,10 +13,13 @@ import (
 	"os"
 )
 
+var recursive bool
+
 func main() {
-	var recursive bool
 	var indexDir string
 	flag.BoolVar(&recursive, "r", false, "Index the directory contents recursively")
+	// flag.BoolVar(&abort, "a", false, "If a file or directory cannot be read during indexing"+
+	//	"terminate immediately")
 	flag.Parse()
 
 	if len(flag.Args()) != 1 {
@@ -30,29 +35,44 @@ func main() {
 	// Try and get FileInfo for the passed directory string
 	fileInfo, err := os.Stat(indexDir)
 	if err != nil {
+		// TODO: improve error message
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	if fileInfo.IsDir() {
 		// handle files in directory
-		files, err := ioutil.ReadDir(fileInfo.Name())
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		for _, fInfo := range files {
-			fmt.Printf("Directory %s contains: %s\n", fileInfo.Name(), fInfo.Name())
-		}
+		readDirectory(fileInfo)
 	} else {
-		fmt.Printf("Reading file: %s\n", fileInfo.Name())
-		contents, err := ioutil.ReadFile(fileInfo.Name())
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		fmt.Printf("File %s contains: %s\n", fileInfo.Name(), contents)
+		readFile(fileInfo)
 	}
+}
+
+func readDirectory(fileInfo os.FileInfo) {
+	files, err := ioutil.ReadDir(fileInfo.Name())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, subFileInfo := range files {
+		if subFileInfo.IsDir() {
+			if recursive {
+				readDirectory(subFileInfo)
+			}
+		} else {
+			readFile(subFileInfo)
+		}
+	}
+}
+
+func readFile(fileInfo os.FileInfo) {
+	fmt.Printf("Reading file: %s\n", fileInfo.Name())
+	contents, err := ioutil.ReadFile(fileInfo.Name())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("File %s contains: %s\n", fileInfo.Name(), contents)
 }
 
 func usage() {
